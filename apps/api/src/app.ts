1dd9b1533bffb7fcfaa1a669c0522d01e789b8e1
vehikl-audience <audience@vehikl.com>
2022-03-04 16:35:02 -0500

    determine namespace of game

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -4,8 +4,13 @@ import express from "express";
 import cors from "cors";
 import globalErrorMiddleware from "./middleware/GlobalErrorMiddleware";
 import { GamesRouter } from "./games/GamesRouter";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {});
 
 app.use(cors());
 app.use(express.json());
@@ -19,4 +24,21 @@ app.use(GamesRouter);
 
 app.use(globalErrorMiddleware);
 
-export default app;
+// io.on("connection", (socket) => {
+//   console.log("we connected boyz");
+// });
+
+const regex = /\/(.*)/;
+
+const namespace = io.of(regex);
+
+const games = [];
+
+namespace.on("connection", (socket) => {
+  const name = socket.nsp.name.match(regex)[1];
+  games.push({ [name]: socket.client });
+
+  console.log(games);
+});
+
+export default httpServer;
